6ab938dbfbe58ea4380e35c947b86f8401f311f3
Anthony Sosso <asosso@friendeo.com>
2012-11-22 16:34:07 -0500

    Tests for game and user creation

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from game_constants import DEFAULT_STARTING_MONEY
 from sqlalchemy import Column, Integer, VARCHAR, INTEGER
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.associationproxy import association_proxy
@@ -5,14 +6,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref, scoped_session
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.schema import ForeignKey
-from sqlalchemy.types import String
+from sqlalchemy.types import String, Boolean
 import dbutils
+import os
 
-#engine = create_engine('mysql://bfc1ffabdb36c3:65da212b@us-cdbr-east-02.cleardb.com/heroku_1cec684f35035ce', echo=True, pool_recycle=3600)#recycle connection every hour to prevent overnight disconnect)
-engine = create_engine('mysql://anthony:fr13nd30@localhost:3306/assassins', echo=True, pool_recycle=3600)#recycle connection every hour to prevent overnight disconnect)
+if bool(os.environ.get('TEST_RUN', False)):
+    engine = create_engine('mysql://anthony:password@localhost:3306/assassins', echo=True, pool_recycle=3600)#recycle connection every hour to prevent overnight disconnect)
+else:
+    engine = create_engine('mysql://bfc1ffabdb36c3:65da212b@us-cdbr-east-02.cleardb.com/heroku_1cec684f35035ce', echo=True, pool_recycle=3600)#recycle connection every hour to prevent overnight disconnect)
+
 Base = declarative_base(bind=engine)
 sm = sessionmaker(bind=engine, autoflush=True, autocommit=False, expire_on_commit=False)
 Session = scoped_session(sm)
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -27,28 +33,37 @@ class User(Base):
         self.password = password
         self.profile_picture = profile_picture
 
-#    item_completions = relationship('ItemCompletion', backref='user') #one to many
-#    completed_items = association_proxy('completed_items', 'item')
-    # user.completed_items.append(foo_item)
 
-class ItemCompletion(Base):
-    __tablename__ = 'itemcompletion'
+class Game(Base):
+    __tablename__ = 'game'
+    #column definitions
+    id = Column(u'id', INTEGER(), primary_key=True, nullable=False)
+    title = Column(u'title', VARCHAR(length=255), nullable=False)
+    password = Column(u'password', VARCHAR(length=255), nullable=False)
+    starting_money = Column(u'starting_money', Integer(), nullable=False)
+    
+    def Game(self, password, title, starting_money=DEFAULT_STARTING_MONEY):
+        self.title = title
+        self.password = password
+        self.starting_money = starting_money
+
+
+class UserInGameStatus(Base):
+    __tablename__ = 'usergamepermission'
 
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    item_id = Column(Integer, ForeignKey('item.id'), primary_key=True)
-    file_path = Column(VARCHAR(255))
+    game_id = Column(Integer, ForeignKey('game.id'), primary_key=True)
+    money = Column(Integer, nullable=False)
+    alive = Column(Boolean)
+    target_user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    
 
-#    user = relationship(User, backref=backref("completed_items", cascade="all, delete-orphan"))
-#    item = relationship("Item")
-    #Session().Query(Item).filter_by(item.id==self.item_id)
-
-    def __init__(self, user_id, item_id, file_path=None):
+    def __init__(self, user_id, item_id, money=DEFAULT_STARTING_MONEY):
         self.user_id = user_id
         self.item_id = item_id
-        self.file_path = file_path
 
     def __repr__(self):
-        return '<ItemCompletion %d @ %d>' % (self.user_id, self.item_id)
+        return '<UserInGameStatus %d @ %d>' % (self.game_id, self.user_id)
 
 
 class Item(Base):
--- test_models.py
+++ test_models.py
@@ -1,21 +1,42 @@
-from models import User, Session
+from models import User, Game, Session, engine, Base
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm.session import sessionmaker
 import unittest
-import bcrypt
 
-class TestUser(unittest.TestCase):
-    
+class BaseTest(unittest.TestCase):
     def teardown(self):
         Session.remove()
-        
+
+class TestUser(BaseTest):
+    
     def test_user_creation(self):
         session = Session()
         u = User(username='Hitman', password='123456', profile_picture='hitman.png')
         session.add(u)
         self.assertEqual(1, len(session.query(User).filter_by(username='Hitman').all()))
+
+class TestGame(BaseTest):
+    
+    def test_game_creation(self):
+        session = Session()
+        users_list = make_users(4)
+        session.add_all(users_list)
+        game = Game(title='test game', password='testpassword', starting_money=3)
+        session.add(game)
+        games_from_db = session.query(Game).all()
+        self.assertEqual(1, len(games_from_db))
+        self.assertEqual(game, games_from_db[0])
         
+        
+def make_users(number_of_users):
+    users = []
+    for user_number in range(number_of_users):
+        users.append(User(username='Hitman'+str(user_number), password='123456', profile_picture='hitman.png'))
+    return users
+
 if __name__ == '__main__':
     unittest.main()
+    Base.metadata.drop_all()
+    

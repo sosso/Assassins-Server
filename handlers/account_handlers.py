343a6117d17d7c5a69cffff52bb33cab93b7cb21
Anthony Sosso <xtremerunnerars@gmail.com>
2012-12-09 19:49:36 -0500

    login logging

--- handlers/account_handlers.py
+++ handlers/account_handlers.py
@@ -10,9 +10,12 @@ class LoginHandler(BaseHandler):
     @auth_required
     @tornado.web.asynchronous
     def post(self):
+        logger = logging.getLogger('CreateUserHandler')
         try:
+            logger.info('Login request: %s %s' % (self.get_argument('username'), self.get_argument('password')))
             result_dict = get_response_dict(True)
         except Exception as e:
+            logger.exception(e)
             self.session.rollback()
             result_dict = get_response_dict(False, e.message)
         finally:
